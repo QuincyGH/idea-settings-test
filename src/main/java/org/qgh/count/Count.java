eed2e0f2c6b8142ea820d95c7653d594917c6ae3
QuincyGH <415553576@qq.com>
2025-05-20 12:01:43 +0800

    添加计算1-100的和

---
+++ src/main/java/org/qgh/count/Count.java
@@ -0,0 +1,11 @@
+package org.qgh.count;
+
+public class Count {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}

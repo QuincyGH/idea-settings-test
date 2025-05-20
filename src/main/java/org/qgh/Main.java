ff93add7981b3bd2dff8d1ec39cdb388e7a8d3d3
QuincyGH <415553576@qq.com>
2025-05-20 12:24:01 +0800

    修改代码注释

--- src/main/java/org/qgh/Main.java
+++ src/main/java/org/qgh/Main.java
@@ -4,13 +4,10 @@ package org.qgh;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // 打印开始标记
         System.out.println("Hello and welcome!");
-
+        // 循环打印数字1~5
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
         // 打印完结标记

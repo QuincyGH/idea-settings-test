952903f5bbd3c673833e05a3eaaec62e34f593b4
QuincyGH <415553576@qq.com>
2025-05-26 14:53:21 +0800

    初始化项目

---
+++ src/main/java/com/qgh/idea_settings_test/IdeaSettingsTestApplication.java
@@ -0,0 +1,13 @@
+package com.qgh.idea_settings_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaSettingsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaSettingsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qgh/idea_settings_test/IdeaSettingsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qgh.idea_settings_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaSettingsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

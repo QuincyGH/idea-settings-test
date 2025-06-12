fc32a20a7881b79637afcb666d468f51537dcdc9
QuincyGH <415553576@qq.com>
2025-06-12 09:32:59 +0800

    添加类文档注释

--- src/main/java/com/qgh/idea_settings_test/company/controller/CompanyController.java
+++ src/main/java/com/qgh/idea_settings_test/company/controller/CompanyController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 企业模块控制层
+ */
 @RestController
 @RequestMapping("/company")
 public class CompanyController {
--- src/main/java/com/qgh/idea_settings_test/company/entity/Company.java
+++ src/main/java/com/qgh/idea_settings_test/company/entity/Company.java
@@ -2,6 +2,9 @@ package com.qgh.idea_settings_test.company.entity;
 
 import lombok.Data;
 
+/**
+ * 企业基础类
+ */
 @Data
 public class Company {
     public int Id;
